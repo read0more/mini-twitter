7bd33df56eef10d13d4bc199784aeb22f1f693da
read0more <read0more@gmail.com>
2023-04-10 18:20:05 +0900

    :sparkles: create tweet

---
+++ src/pages/api/tweet/index.ts
@@ -0,0 +1,79 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prismaClient from '@/libs/server/prismaClient';
+import withHandler, { ResponseType } from '@/libs/server/withHandler';
+import z from 'zod';
+import { withApiSession } from '@/libs/server/withSession';
+
+const schema = z.object({
+  text: z.string().min(1, '내용을 입력해주세요.'),
+});
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  if (req.method === 'POST') {
+    const id = req.session.user?.id ?? 0;
+    try {
+      const result = schema.parse(req.body);
+      const user = await prismaClient.user.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!user) {
+        return res.status(401).json({
+          ok: false,
+          error: '로그인이 필요합니다.',
+        });
+      }
+
+      const tweet = await prismaClient.tweet.create({
+        data: {
+          text: result.text,
+          user: {
+            connect: {
+              id,
+            },
+          },
+        },
+      });
+
+      return res.status(200).json({
+        ok: true,
+        tweet,
+      });
+    } catch (e) {
+      if (e instanceof z.ZodError) {
+        return res.status(400).json({
+          ok: false,
+          error: e.issues,
+        });
+      }
+
+      return res.status(500).json({
+        ok: false,
+        error: 'Unknown error',
+      });
+    }
+  } else {
+    // TODO: pagination
+    const tweets = await prismaClient.tweet.findMany({
+      take: 10,
+      skip: 0,
+    });
+
+    return res.status(200).json({
+      ok: true,
+      tweets,
+    });
+  }
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ['POST', 'GET'],
+    handler,
+  })
+);
