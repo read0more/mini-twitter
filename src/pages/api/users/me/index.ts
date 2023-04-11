ea3748080c52ee265a8bedf86182182532640143
read0more <read0more@gmail.com>
2023-04-11 16:34:04 +0900

    :sparkles: change name API

--- src/pages/api/users/me/index.ts
+++ src/pages/api/users/me/index.ts
@@ -2,27 +2,63 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import prismaClient from '@/libs/server/prismaClient';
 import withHandler, { ResponseType } from '@/libs/server/withHandler';
 import { withApiSession } from '@/libs/server/withSession';
+import { z } from 'zod';
+
+const schema = z.object({
+  name: z.string(),
+});
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const id = req.session.user?.id;
-  const user = await prismaClient.user.findUnique({
-    where: {
-      id,
-    },
-  });
+  if (req.method === 'GET') {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        id,
+      },
+    });
 
-  return res.status(200).json({
-    ok: true,
-    user,
-  });
+    return res.status(200).json({
+      ok: true,
+      user,
+    });
+  } else if (req.method === 'POST') {
+    try {
+      const { name } = schema.parse(req.body);
+      const user = await prismaClient.user.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+        },
+      });
+
+      return res.status(200).json({
+        ok: true,
+        user,
+      });
+    } catch (e) {
+      if (e instanceof z.ZodError) {
+        return res.status(400).json({
+          ok: false,
+          error: e.issues,
+        });
+      }
+
+      return res.status(500).json({
+        ok: false,
+        error: 'Unknown error',
+      });
+    }
+  }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ['GET'],
+    methods: ['GET', 'POST'],
     handler,
   })
 );
